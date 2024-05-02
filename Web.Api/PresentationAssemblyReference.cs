3073b3572a65c2eb1027cbdbf4bbd5410134ecd7
Cristian Barbosa <cbarbosa@ccl.com.co>
2024-05-02 14:35:42 -0500

    Ajustes para subir a la base de datos

---
+++ Web.Api/PresentationAssemblyReference.cs
@@ -0,0 +1,8 @@
+namespace Web.Api;
+
+using System.Reflection;
+
+public class PresentationAssemblyReference
+{
+    internal static readonly Assembly Assembly = typeof(PresentationAssemblyReference).Assembly;
+}
